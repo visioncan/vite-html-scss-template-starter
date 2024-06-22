45bd86b47fa1658c918516473d18861fd1934ee5
visioncan <visioncan@gmail.com>
2024-06-22 23:08:58 +0800

    infra

---
+++ README.md
@@ -0,0 +1,13 @@
+# HTML + SCSS Development
+
+Simple HTML SCSS Template use Vite!
+
+## Scripts
+
+```bash
+# 開發模式
+npm run dev
+
+# 譯譯
+npm run build
+```
---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import path from 'path'
+import glob from 'fast-glob'
+import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
+
+export default defineConfig({
+  server: {
+    port: 3000
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  css: {
+    devSourcemap: true // sourcemap
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // assetFileNames: 'assets/[name][extname]' // 不帶版號
+      },
+      input: Object.fromEntries(
+        glob
+          .sync(['./*.html', './pages/**/*.html'])
+          .map(file => [
+            path.relative(__dirname, file.slice(0, file.length - path.extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url))
+          ])
+      )
+    }
+  }
+})
